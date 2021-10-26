03ac14141fcbebfe972d64d904a4376d663389d7
arul2101 <myanuarullah20@gmail.com>
2021-10-27 06:56:53 +0700

    Membuat function queryLoop untuk handle looping tabel data_pegawai

--- myfunctions/functions.php
+++ myfunctions/functions.php
@@ -5,4 +5,28 @@
     // Koneksi ke database
     $db = mysqli_connect("localhost", "root", "", "latihan");
 
+    // Function untuk looping tabel data_pegawai
+    function queryLoop($query) {
+
+        // Global Scope
+        global $db;
+
+        // Ambil data (fetch) dari tabel data_pegawai
+        $result = mysqli_query($db, $query);
+
+        // Bikin array kosong untuk wadahnya
+        $rows = [];
+
+        // Buat pengulangan untuk menampilkan perbaris dari tabel data_pegawai
+        while( $row = mysqli_fetch_assoc($result) ) {
+
+        // Setiap baris pengulangannya, masuk ke array kosong yang sudah dibuat
+        $rows[] = $row;
+
+        }
+
+        return $rows;
+        
+    }
+
 ?>
